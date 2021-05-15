821eda5e35f5dd8fdb0264a5c70402c7f73984b3
Joao Pedro <jpcar200@gmail.com>
2021-05-15 13:22:55 -0300

    rotas alterada

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-  } from "react-router-dom";
+// import React from 'react'
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route
+//   } from "react-router-dom";
 
-import Musicas from './componets/main/musicas/Musicas'
-import Fotos from './componets/main/fotos/Fotos'
-import Noticias from './componets/main/noticias/Noticias'
+// import Musicas from './componets/main/musicas/Musicas'
+// import Fotos from './componets/main/fotos/Fotos'
+// import Noticias from './componets/main/noticias/Noticias'
 
-export default function mainRoutes() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" element={<Musicas/>}/> 
-                <Route path="/Musicas" element={<Musicas/>}/>
-                <Route path="/Fotos" element={<Fotos/>}/>
-                <Route path="/Noticias" element={<Noticias/>}/>
-            </Switch>
-        </Router>
-    );
-}
+// export default function mainRoutes() {
+//     return (
+//         <Router>
+//             <Switch>
+//                 <Route path="/" element={<Musicas/>}/> 
+//                 <Route path="/Musicas" element={<Musicas/>}/>
+//                 <Route path="/Fotos" element={<Fotos/>}/>
+//                 <Route path="/Noticias" element={<Noticias/>}/>
+//             </Switch>
+//         </Router>
+//     );
+// }
